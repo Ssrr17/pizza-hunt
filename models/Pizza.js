245dcb09a30bfa36f345b7145a44141a64d06734
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-05-06 15:48:02 -0400

    date formatter added to schema:

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,25 +1,31 @@
-const { Schema, model } = require('mongoose');
+const dateFormat = require("../utils/dateFormat");
+const { Schema, model } = require("mongoose");
 
 const PizzaSchema = new Schema({
-    pizzaName: {
-      type: String
-    },
-    createdBy: {
-      type: String
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now
-    },
-    size: {
-      type: String,
-      default: 'Large'
-    },
-    toppings: []
-  });
+  pizzaName: {
+    type: String,
+  },
+  createdBy: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal),
+  },
+  size: {
+    type: String,
+    default: "Large",
+  },
+  toppings: [],
+  toJSON: {
+    virtuals: true,
+    getters: true
+  },
+});
 
 // create the Pizza model using the PizzaSchema
-const Pizza = model('Pizza', PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 
 // export the Pizza model
 module.exports = Pizza;
